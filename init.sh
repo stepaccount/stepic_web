c193e21f71dd8bf223b8f8cb8e79d50242169849
Aquila <aquila@eri.av>
2016-11-09 11:10:34 +0300

    Изменение конфига и скрипта

--- init.sh
+++ init.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+#sudo rm /etc/nginx/sites-enabled/test.conf
+#sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
