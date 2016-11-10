8fe35a8b92162cb3b1ecec8a208268a50e75b698
Aquila <aquila@eri.av>
2016-11-10 09:58:15 +0300

    Исправления в hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
 
 def app(env, start_response):
     url_param_str = env['QUERY_STRING'].replace("&","\n")
+    if url_param_str != "":
+        url_param_str += "\n"
     start_response('200 OK', [('Content-Type', 'text/plain')])
     return [url_param_str]
