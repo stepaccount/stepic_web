ca5b0aa84d0f56fe29ebe5e3bcee44fe951ee4c3
Aquila <aquila@eri.av>
2016-11-15 13:14:06 +0300

    Исправление DEBUG

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = '_pz)d)l-(salc@-wi6ll_j4)+_*ch2@e9df_*b(@_!zu&%09=9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = false
+DEBUG = False
 
 TEMPLATE_DEBUG = True
 
