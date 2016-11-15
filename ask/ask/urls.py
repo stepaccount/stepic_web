b1923d6c80ce28f3f908b239c3e2c2fad4716386
Aquila <aquila@eri.av>
2016-11-15 13:04:05 +0300

    Реализация основы приложения qa

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,12 +1,20 @@
+# -*- coding: utf-8 -*-
+
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns('qa.views',
     # Examples:
     # url(r'^$', 'ask.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/.*$', 'test', name = 'login'),
+    url(r'^signup/.*$', 'test', name = 'signup'),
+    url(r'^question/(?P<id>[0-9]+)/$', 'test', name = 'question'),
+    url(r'^ask/.*$', 'test', name = 'ask'),
+    url(r'^popular/.*$', 'test', name = 'polular'),
+    url(r'^new/.*$', 'test', name = 'new')
 )
+
--- init.sh
+++ init.sh
@@ -7,5 +7,8 @@ sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 #sudo rm /etc/gunicorn.d/hello.py
 #sudo ln -s /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
+sudo killall -SIGTERM gunicorn
+sudo gunicorn -b 0.0.0.0:8080 --pythonpath /home/box/web/ hello:app &
+sudo gunicorn -b 0.0.0.0:8000 --pythonpath /home/box/web/ask ask.wsgi:application &
 #sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/mysql start
